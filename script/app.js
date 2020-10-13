b56d8e104d45df5c4166d641dfbe3ed50d8066c2
DemetsJelle <55786744+DemetsJelle@users.noreply.github.com>
2020-10-13 18:41:20 +0200

    floating label + hartje animation

--- script/app.js
+++ script/app.js
@@ -1,18 +1,114 @@
-function handleFloatingLabel() {}
+let email = {},
+    password = {},
+    signInButton;
+
+const getDOMlements = function() {
+    email.input = document.querySelector(".js-email-input");
+    email.errorMessage = document.querySelector(".js-email-message");
+    email.field = document.querySelector(".js-email-field");
+
+    password.input = document.querySelector(".js-password-input");
+    password.errorMessage = document.querySelector(".js-password-message");
+    password.field = document.querySelector(".js-password-field");
+
+    signInButton = document.querySelector(".js-sign-in-button");
+    enableListeners();
+};
+
+const doubleCheckEmail = function()  {
+    if (!isEmpty(email.input.value) && !isValid(email)) {
+        addErrors(email, "Invalid email");
+    } else {
+        removeErrors(email);
+    }
+};
+const doubleCheckPassword = function()  {
+    if (!isEmpty(password.input.value) && !isValid(password)) {
+        addErrors(password, "Invalid password");
+    } else {
+        removeErrors(password);
+    }
+};
+
+const enableListeners = function() {
+    email.input.addEventListener("blur", () => {
+        if (!isValid(email)) {
+            email.input.addEventListener("input", doubleCheckEmail);
+            isEmpty(email.input.value) == false ?
+                addErrors(email, "Invalid email") :
+                addErrors(email, "This field is required");
+        } else {
+            email.input.removeEventListener("input", doubleCheckEmail);
+        }
+    });
+    password.input.addEventListener("blur", () => {
+        if (!isValid(password)) {
+            password.input.addEventListener("input", doubleCheckPassword);
+            isEmpty(password.input.value) == false ?
+                addErrors(password, "Invalid password") :
+                addErrors(password, "This field is required");
+        } else {
+            password.input.removeEventListener("input", doubleCheckPassword);
+        }
+    });
+    signInButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        if ((isValid(email) && !isEmpty(email.input.value)) && (isValid(password) && !isEmpty(password.input.value))) {
+            console.log(email.input.value);
+            console.log(password.input.value);
+        }
+    });
+};
+
+const addErrors = function(obj, message) {
+    obj.message.innerHTML = message;
+    obj.field.classList.add("has-error");
+};
+const removeErrors = function(obj) {
+    obj.message.innerHTML = "";
+    obj.field.classList.remove("has-error");
+};
+
+const isValid = function(obj) {
+    if (obj === email) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(obj.input.value);
+    } else if (obj === password) {
+        return obj.input.value.length > 1;
+    } else {
+        return false;
+    }
+};
+
+const isEmpty = function(fieldValue) {
+    return !fieldValue || !fieldValue.length;
+};
+
+const handleFloatingLabel = () => {
+    
+
+};
 
 function handlePasswordSwitcher() {
-  // De + operator doet een bitoperatie en zet de true, false van checked om naar 0 en 1. Daarmee kan je een item uit de positie van de array ophalen (aan de hand van de index).
-  // Dit is niet echt heel leesbaar, maar je kan heel kort een if-else vermijden.
-  // Let er wel op dat de leesbaarheid van code altijd goed blijft.
+    
+  let checkbox = document.querySelector(`.c-toggle-password__checkbox`);
+  let passwordInput = document.querySelector(`.c-toggle-password__input`);
+  checkbox.addEventListener(`change`, function(){
 
-  const passwordOptions = ["password", "text"];
-  passwordToggle.addEventListener("change", function () {
-    passwordInput.type = passwordOptions[+this.checked];
-  });
+      if(checkbox.checked){
+          passwordInput.type = "password";
+          
+          console.log("checked")
+      }
+      else{
+          passwordInput.type = "text";
+          console.log("unchecked")
+      }
+  })
+
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("Script loaded!");
-  handleFloatingLabel();
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('Script loaded!');
+  //handleFloatingLabel();
   handlePasswordSwitcher();
 });
